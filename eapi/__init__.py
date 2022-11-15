d7b7b6b2fa702d40383c8ce54d854fd84de530b7
Jesse Mather <Jesse.Mather10@t-mobile.com>
2022-11-15 09:45:08 -0800

    pylint and version bump

--- eapi/__init__.py
+++ eapi/__init__.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2020 Arista Networks, Inc.  All rights reserved.
 # Arista Networks, Inc. Confidential and Proprietary.
+"""
+eapi
+~~~~~~~~~~~~~~~~
 
-__version__ = "0.6.4"
+"""
 
-# from eapi.constants import ENCODING, INCLUDE_TIMESTAMPS, SSL_VERIFY, \
-#                          SSL_WARNINGS, TIMEOUT
+__version__ = "0.7.0"
 
 import eapi.environments
 import eapi.types
 
 from eapi.sessions import Session, AsyncSession
 from eapi.api import aexecute, awatch, configure, enable, execute, watch
-from eapi.exceptions import EapiError, EapiHttpError, EapiPathNotFoundError, EapiResponseError, EapiTimeoutError
+from eapi.exceptions import (EapiError, EapiHttpError, EapiPathNotFoundError,
+    EapiResponseError, EapiTimeoutError)
