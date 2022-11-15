6717f9b0231a0ffacf8e0eea811501da0df76aa0
Jesse Mather <Jesse.Mather10@t-mobile.com>
2022-11-14 20:49:34 -0800

    add prepare_command

--- eapi/util.py
+++ eapi/util.py
@@ -4,14 +4,11 @@
 
 import os
 import uuid
-
+import dataclasses
 from typing import Optional, Union, List
-
-import eapi.sessions
 from eapi.types import Command, Params, Request
 
 from eapi.environments import EAPI_DEFAULT_ENCODING
-
 
 def clear_screen() -> None:
     if os.name == 'nt':
@@ -28,47 +25,33 @@ def indent(spaces, text: str):
     return "\n".join(indented)
 
 
-def prepare_cmd(commands: Union[Command, List[Command]]):
-
-    if isinstance(commands, (str, dict)):
-        return prepare_cmd([commands])
-
-    prepared = []
-    for cmd in commands:
-        if isinstance(cmd, str):
-            prepared.append({"cmd": cmd, "input": ""})
-        else:
-            prepared.append(cmd)
-
-    return prepared
-
-
-def prepare_request(commands: List[Command], encoding: Optional[str] = None, streaming: bool = False) -> Request:
-    commands = prepare_cmd(commands)
+def prepare_request(commands: List[Union[str, Command]], 
+        encoding: Optional[str] = None, streaming: bool = False) -> Request:
+    #commands = prepare_cmd(commands)
     request_id = str(uuid.uuid4())
 
     if not encoding:
         encoding = EAPI_DEFAULT_ENCODING
 
-    params: Params = {
-        "version": 1,
-        "format": encoding,
-        "cmds": commands
-    }
+    _commands = []
+    for c in commands:
+        if isinstance(c, str):
+            c = Command(cmd=c)
+        _commands.append(c)
+    
+    params = Params(
+        version = 1,
+        format = encoding,
+        cmds = _commands
+    )
 
-    # 'timestamps': bool,
-	# 'auto_complete': bool,
-	# 'expand_aliases': bool,
-	# 'include_error_detail': bool,
-	# 'streaming': bool # not support until 4.24
-
-    req: Request = {
-        "jsonrpc": "2.0",
-        "method": "runCmds",
-        "params": params,
-        "streaming": streaming,
-        "id": request_id
-    }
+    req: Request = Request(
+        jsonrpc = "2.0",
+        method = "runCmds",
+        params = params,
+        streaming = streaming,
+        id = request_id
+    )
     return req
 
 
@@ -84,3 +67,19 @@ def zpad(keys, values, default=None):
     values += [default] * (keys_len - values_len)
 
     return zip(keys, values)
+
+def pruned_dict(data):
+    pruned = {}
+    for key, val in data:
+        if val is not None:
+            pruned[key] = val
+
+    return pruned
+
+def asdict(data, prune=True):
+    """Normal `asdict` but removes fields with None values"""
+
+    factory = pruned_dict if prune else dict
+
+    return dataclasses.asdict(data, dict_factory=factory)
+    
